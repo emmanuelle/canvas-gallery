eb3757baa3a96fed58157115cf92b1dabca919cb
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2019-03-24 21:45:11 +0100

    modified app1

--- app1_seg.py
+++ app1_seg.py
@@ -110,69 +110,38 @@ layout = html.Div([
 
 def callbacks(app):
     @app.callback(Output('segmentation', 'figure'),
-                [Input('canvas', 'image_content'),
-                Input('canvas', 'json_data'),
-                Input('canvas', 'height')],
-                [State('canvas', 'scale'),
-                State('canvas', 'width'),
+                [Input('canvas', 'json_data')],
+                [State('canvas', 'image_content'),
                 State('algorithm', 'value')])
-    def update_figure_upload(image, string, h, s, w, algorithm):
-        mask = parse_jsonstring(string, shape=(round(h/s), round(w/s)))
-        if mask.sum() > 0:
+    def update_figure_upload(string, image, algorithm):
+        print("update figure")
+        if string:
             if image is None:
                 im = img
                 image = img
             else:
                 im = image_string_to_PILImage(image)
                 im = np.asarray(im)
-            seg = segmentation_generic(im, mask, mode=algorithm)
+            shape = im.shape[:2]
+            mask = parse_jsonstring(string, shape=shape)
+            if mask.sum() > 0:
+                seg = segmentation_generic(im, mask, mode=algorithm)
+            else:
+                seg = np.zeros(shape)
+            return image_with_contour(im, seg, shape=shape)
         else:
-            if image is None:
-                image = img
-            seg = np.zeros((h, w))
-        return image_with_contour(image, seg, shape=(round(h/s), round(w/s)))
+            raise PreventUpdate
 
 
 
     @app.callback(Output('canvas', 'image_content'),
                 [Input('upload-image', 'contents')])
     def update_canvas_upload(image_string):
+        print("uploading", image_string[:100])
         if image_string is None:
             raise PreventUpdate
-        if image_string is not None:
+        else:
             return image_string
-        else:
-            return None
-
-
-    @app.callback(Output('canvas', 'height'),
-                [Input('upload-image', 'contents')],
-                [State('canvas', 'width'),
-                State('canvas', 'height')])
-    def update_canvas_upload_shape(image_string, w, h):
-        if image_string is None:
-            raise PreventUpdate
-        if image_string is not None:
-            # very dirty hack, this should be made more robust using regexp
-            im = image_string_to_PILImage(image_string)
-            im_h, im_w = im.height, im.width
-            return round(w / im_w * im_h)
-        else:
-            return canvas_height
-
-
-    @app.callback(Output('canvas', 'scale'),
-                [Input('upload-image', 'contents')])
-    def update_canvas_upload_scale(image_string):
-        if image_string is None:
-            raise PreventUpdate
-        if image_string is not None:
-            # very dirty hack, this should be made more robust using regexp
-            im = image_string_to_PILImage(image_string)
-            im_h, im_w = im.height, im.width
-            return canvas_width / im_w
-        else:
-            return scale
 
 
     @app.callback(Output('segmentation-tabs', 'value'),
@@ -181,4 +150,3 @@ def callbacks(app):
         if string:
             return 'segmentation-result-tab'
         return 'segmentation-canvas-tab'
-
